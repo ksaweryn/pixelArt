0046f3ae16414a1a39097a52f9431812484f2f9c
Javier Borja <javier.borja@edmodo.com>
2022-08-27 20:11:11 -0500

    Add initial reset functionality

--- main.js
+++ main.js
@@ -4,28 +4,24 @@ const DEFAULT_GRID_SIZE = 20;
 const PIXEL_CLASS_NAME = 'pixel';
 const canvas = document.querySelector('.canvas');
 const numberInput = document.querySelector('.numberInput');
-const pixelEl = document.querySelector(`.${PIXEL_CLASS_NAME}`);
 const colorEl = document.querySelector('.colorInput');
-
-const removeChildrenNodes = () => {
-  let child = canvas.lastElementChild;
-  while (child) {
-    canvas.removeChild(child);
-    child = canvas.lastElementChild;
-  }
-};
 
 const gridDrawer = (size) => {
   if (size > 100 || size <= 0) return;
-
   canvas.style.setProperty('--canvasSize', size);
-  removeChildrenNodes();
+  canvas.innerHTML = '';
 
   for (let i = 1; i <= size * size; i++) {
     const pixel = document.createElement('div');
     pixel.className = PIXEL_CLASS_NAME;
     canvas.appendChild(pixel);
   }
+};
+
+const resetCanvas = () => {
+  canvas.innerHTML = '';
+  numberInput.value = DEFAULT_GRID_SIZE;
+  gridDrawer(DEFAULT_GRID_SIZE);
 };
 
 numberInput.addEventListener('keyup', () => {
@@ -36,4 +32,4 @@ numberInput.addEventListener('change', () => {
   gridDrawer(numberInput.value);
 });
 
-gridDrawer(20);
+gridDrawer(DEFAULT_GRID_SIZE);
